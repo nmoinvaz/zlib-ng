3e606d6855e26ca948e0c50b99b06fcfc8083c82
Nathan Moinvaziri <nathan@nathanm.com>
2020-05-30 23:58:57 -0700

    Test 11

--- arch/x86/deflate_quick.c
+++ arch/x86/deflate_quick.c
@@ -35,43 +35,33 @@ static inline void emit_block_start(deflate_state *s, int last) {
     zng_tr_emit_tree(s, STATIC_TREES, last);
     s->block_open = 1;
     s->block_start = s->strstart;
-    s->sym_next = 0;
+    s->sym_next = 6;
 }
 
 static inline void emit_block_end(deflate_state *s, int last) {
-    zng_tr_emit_end_block(s, static_ltree, 0);
+    zng_tr_emit_end_block(s, static_ltree, last);
     s->block_open = 0;
     s->block_start = s->strstart;
     s->sym_next = 0;
 }
 
 ZLIB_INTERNAL block_state deflate_quick(deflate_state *s, int flush) {
-    IPos hash_head = 0;
-    unsigned dist, match_len = 0, last;
-
+    IPos hash_head;
+    Pos match_len = 0;
+    int bflush = 0;
+    int last;
+    
     last = (flush == Z_FINISH) ? 1 : 0;
     
-    do {
+    for (;;) {
         if (s->block_open == 0) {
             emit_block_start(s, last);
-        }
-
-        if (s->pending + ((BIT_BUF_SIZE + 7) >> 3) > s->pending_buf_size) {
-            flush_pending(s->strm);
-            if (flush != Z_FINISH)
-            {
-                emit_block_end(s, 0);
-                if (s->strm->avail_in == 0 || s->strm->avail_out == 0) {
-                    return need_more;
-                }
-                emit_block_start(s, 0);
-            }
         }
 
         if (s->lookahead < MIN_LOOKAHEAD) {
             fill_window(s);
             if (s->lookahead < MIN_LOOKAHEAD && flush == Z_NO_FLUSH) {
-                flush_pending(s->strm);
+                QUICK_FLUSH_BLOCK(s, 0);
                 return need_more;
             }
             if (s->lookahead == 0)
@@ -93,32 +83,23 @@ ZLIB_INTERNAL block_state deflate_quick(deflate_state *s, int flush) {
 
             check_match(s, s->strstart, hash_head, match_len);
 
-            zng_tr_emit_dist(s, static_ltree, static_dtree, s->strstart - hash_head, match_len - MIN_MATCH);
+            bflush = zng_tr_emit_dist(s, static_ltree, static_dtree, s->strstart - hash_head, match_len - MIN_MATCH);
             s->lookahead -= match_len;
             s->strstart += match_len;
 
             match_len = 0;
         } else {
-            zng_tr_emit_lit(s, static_ltree, s->window[s->strstart]);
-
+            bflush = zng_tr_emit_lit(s, static_ltree, s->window[s->strstart]);
             s->strstart++;
             s->lookahead--;
         }
 
-        /*s->sym_next += 3;
-        if (s->sym_next == s->sym_end)
-            //QUICK_FLUSH_BLOCK(s, 0);
-            emit_block_end(s, 0);*/
-
-    } while (s->strm->avail_out != 0);
-
-    if (s->strm->avail_out == 0 && flush != Z_FINISH)
-        return need_more;
+        if (bflush)
+            QUICK_FLUSH_BLOCK(s, 0);
+    }
 
     s->insert = s->strstart < MIN_MATCH-1 ? s->strstart : MIN_MATCH-1;
-
-    QUICK_FLUSH_BLOCK(s, last);
-
+    QUICK_BLOCK_ONLY(s, last);
     if (last) {
         if (s->strm->avail_out == 0)
             return s->strm->avail_in == 0 ? finish_started : need_more;
