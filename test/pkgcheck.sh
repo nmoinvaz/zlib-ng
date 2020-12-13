adebdd6fabf4f51a4873e5234a5164814e3335d6
Nathan Moinvaziri <nathan@nathanm.com>
2020-12-12 20:37:59 -0800

    No ninja

--- test/pkgcheck.sh
+++ test/pkgcheck.sh
@@ -109,7 +109,7 @@ rm -rf btmp2 pkgtmp2
 mkdir btmp2 pkgtmp2
 export DESTDIR=$(pwd)/pkgtmp2
 cd btmp2
-  cmake -G "Unix Makefiles" ${CMAKE_ARGS} ..
+  cmake ${CMAKE_ARGS} ..
   make
   make install
 cd ..
