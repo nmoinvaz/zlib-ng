9a33a0b40c30d44e761ecddec190e2a534e6072c
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2021-01-25 00:04:36 -0800

    LD

--- test/pkgcheck.sh
+++ test/pkgcheck.sh
@@ -120,7 +120,7 @@ mkdir btmp2 pkgtmp2
 
 export DESTDIR=$(pwd)/pkgtmp2
 cd btmp2
-  cmake -G Ninja ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_CREATE_SHARED_LIBRARY="/usr/bin/xcrun libtool -D -o <TARGET> <LINK_FLAGS> <OBJECTS>"
+  cmake -G Ninja ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_CREATE_SHARED_LIBRARY="ld -D -o <TARGET> <LINK_FLAGS> <OBJECTS>"
   ninja -v
   ninja install
 cd ..
