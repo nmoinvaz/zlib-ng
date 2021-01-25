e9477c25752d9174b9b3b9048bb60c62116d3ce6
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2021-01-24 23:30:54 -0800

    Verbose makefile.

--- test/pkgcheck.sh
+++ test/pkgcheck.sh
@@ -120,7 +120,7 @@ mkdir btmp2 pkgtmp2
 export LD=ld64
 export DESTDIR=$(pwd)/pkgtmp2
 cd btmp2
-  cmake -G Ninja ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON
+  cmake -G Ninja ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON -DCMAKE_VERBOSE_MAKEFILE=ON
   ninja -v
   ninja install
 cd ..
