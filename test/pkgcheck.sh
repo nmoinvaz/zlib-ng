14e1c70b049bf942b24a75c3548307b6c2e7087a
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2021-01-25 00:05:20 -0800

    No Ninja

--- test/pkgcheck.sh
+++ test/pkgcheck.sh
@@ -120,9 +120,9 @@ mkdir btmp2 pkgtmp2
 
 export DESTDIR=$(pwd)/pkgtmp2
 cd btmp2
-  cmake -G Ninja ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_CREATE_SHARED_LIBRARY="ld -D -o <TARGET> <LINK_FLAGS> <OBJECTS>"
-  ninja -v
-  ninja install
+  cmake ${CMAKE_ARGS} .. -DCMAKE_OSX_SYSROOT=$sysroot -DCMAKE_SKIP_RPATH=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_CREATE_SHARED_LIBRARY="ld -D -o <TARGET> <LINK_FLAGS> <OBJECTS>"
+  make
+  make install
 cd ..
 echo "ZERO_AR_DATE = $ZERO_AR_DATE"
 # Original build system
