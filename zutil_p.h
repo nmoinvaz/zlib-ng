9e881e44e482255a5526edeac09fcdc6e53bd87d
Nathan Moinvaziri <nathan@nathanm.com>
2021-04-15 15:15:21 -0700

    Use posix_memalign on macOS.

--- zutil_p.h
+++ zutil_p.h
@@ -22,7 +22,8 @@ static inline void *zng_alloc(size_t size) {
 #elif defined(_WIN32)
     return (void *)_aligned_malloc(size, 64);
 #elif defined(__APPLE__)
-    return (void *)malloc(size);     /* MacOS always aligns to 16 bytes */
+    void *ptr;
+    return posix_memalign(&ptr, 64, size) ? NULL : ptr;
 #else
     return (void *)memalign(64, size);
 #endif
