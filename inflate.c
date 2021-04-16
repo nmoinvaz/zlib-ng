034870f6bb87b3fa9acc46d37ae10f24ea36f2a9
Nathan Moinvaziri <nathan@nathanm.com>
2021-04-15 20:18:53 -0700

    Remove inflate fast.

--- inflate.c
+++ inflate.c
@@ -797,14 +797,14 @@ int32_t Z_EXPORT PREFIX(inflate)(PREFIX3(stream) *strm, int32_t flush) {
 
         case LEN:
             /* use inflate_fast() if we have enough input and output */
-            if (have >= INFLATE_FAST_MIN_HAVE && left >= INFLATE_FAST_MIN_LEFT) {
+            /*if (have >= INFLATE_FAST_MIN_HAVE && left >= INFLATE_FAST_MIN_LEFT) {
                 RESTORE();
                 zng_inflate_fast(strm, out);
                 LOAD();
                 if (state->mode == TYPE)
                     state->back = -1;
                 break;
-            }
+            }*/
             state->back = 0;
 
             /* get a literal, length, or end-of-block code */
