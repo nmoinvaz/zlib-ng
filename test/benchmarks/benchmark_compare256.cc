84891facb776f3d33e0dac0dbaad311035460064
Nathan Moinvaziri <nathan@nathanm.com>
2026-01-08 17:15:01 -0800

    asd

--- test/benchmarks/benchmark_compare256.cc
+++ test/benchmarks/benchmark_compare256.cc
@@ -86,6 +86,9 @@ BENCHMARK_COMPARE256(avx512, compare256_avx512, test_cpu_features.x86.has_avx512
 #if defined(ARM_NEON) && defined(HAVE_BUILTIN_CTZLL)
 BENCHMARK_COMPARE256(neon, compare256_neon, test_cpu_features.arm.has_neon);
 #endif
+#if defined(ARM_SVE)
+BENCHMARK_COMPARE256(sve, compare256_sve, test_cpu_features.arm.has_sve);
+#endif
 #ifdef POWER9
 BENCHMARK_COMPARE256(power9, compare256_power9, test_cpu_features.power.has_arch_3_00);
 #endif
