d8faa14a1f904427577f3a4a33b862b5b3ba9a84
Nathan Moinvaziri <nathan@nathanm.com>
2026-01-08 17:24:04 -0800

    asd

--- arch/arm/compare256_sve.c
+++ arch/arm/compare256_sve.c
@@ -15,40 +15,6 @@ static inline uint32_t compare256_sve_static(const uint8_t *src0, const uint8_t 
     uint64_t vl = svcntb();
     svbool_t pg_all = svptrue_b8();
 
-    while (len + 4 * vl <= 256) {
-        svuint8_t v0 = svld1_u8(pg_all, src0);
-        svuint8_t v1 = svld1_u8(pg_all, src1);
-        svuint8_t v2 = svld1_u8(pg_all, src0 + vl);
-        svuint8_t v3 = svld1_u8(pg_all, src1 + vl);
-        svuint8_t v4 = svld1_u8(pg_all, src0 + 2 * vl);
-        svuint8_t v5 = svld1_u8(pg_all, src1 + 2 * vl);
-        svuint8_t v6 = svld1_u8(pg_all, src0 + 3 * vl);
-        svuint8_t v7 = svld1_u8(pg_all, src1 + 3 * vl);
-
-        svbool_t cmp0 = svcmpne_u8(pg_all, v0, v1);
-        svbool_t cmp1 = svcmpne_u8(pg_all, v2, v3);
-        svbool_t cmp2 = svcmpne_u8(pg_all, v4, v5);
-        svbool_t cmp3 = svcmpne_u8(pg_all, v6, v7);
-
-        svbool_t cmp_01 = svorr_b_z(pg_all, cmp0, cmp1);
-        svbool_t cmp_23 = svorr_b_z(pg_all, cmp2, cmp3);
-        svbool_t cmp_all = svorr_b_z(pg_all, cmp_01, cmp_23);
-
-        if (svptest_any(pg_all, cmp_all)) {
-            if (svptest_any(pg_all, cmp0))
-                return len + (uint32_t)svcntp_b8(pg_all, svbrkb_b_z(pg_all, cmp0));
-            if (svptest_any(pg_all, cmp1))
-                return len + (uint32_t)vl + (uint32_t)svcntp_b8(pg_all, svbrkb_b_z(pg_all, cmp1));
-            if (svptest_any(pg_all, cmp2))
-                return len + (uint32_t)(2 * vl) + (uint32_t)svcntp_b8(pg_all, svbrkb_b_z(pg_all, cmp2));
-            return len + (uint32_t)(3 * vl) + (uint32_t)svcntp_b8(pg_all, svbrkb_b_z(pg_all, cmp3));
-        }
-
-        src0 += 4 * vl;
-        src1 += 4 * vl;
-        len += (uint32_t)(4 * vl);
-    }
-
     while (len + vl <= 256) {
         svuint8_t v0 = svld1_u8(pg_all, src0);
         svuint8_t v1 = svld1_u8(pg_all, src1);
@@ -61,18 +27,20 @@ static inline uint32_t compare256_sve_static(const uint8_t *src0, const uint8_t 
         src0 += vl;
         src1 += vl;
         len += (uint32_t)vl;
-    }
 
-    if (len < 256) {
-        svbool_t pg = svwhilelt_b8_u32(len, 256);
-        svuint8_t v0 = svld1_u8(pg, src0);
-        svuint8_t v1 = svld1_u8(pg, src1);
-        svbool_t cmp = svcmpne_u8(pg, v0, v1);
 
-        if (svptest_any(pg, cmp)) {
-            return len + (uint32_t)svcntp_b8(pg, svbrkb_b_z(pg, cmp));
+        v0 = svld1_u8(pg_all, src0);
+        v1 = svld1_u8(pg_all, src1);
+        cmp = svcmpne_u8(pg_all, v0, v1);
+
+        if (svptest_any(pg_all, cmp)) {
+            return len + (uint32_t)svcntp_b8(pg_all, svbrkb_b_z(pg_all, cmp));
         }
-    }
+
+        src0 += vl;
+        src1 += vl;
+        len += (uint32_t)vl;
+    } while (len < 256);
 
     return 256;
 }
